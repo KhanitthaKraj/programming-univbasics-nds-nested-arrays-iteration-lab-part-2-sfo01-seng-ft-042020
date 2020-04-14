98cc4e255b811f67a9d884faefd260cdf29f4f91
Khanittha Kraj <kkraj.swe@gmail.com>
2020-04-14 06:39:09 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,14 +4,15 @@ def find_min_in_nested_arrays(src)
   
   count = 0
   low_temp = []
-  
+  min = []
   while count < src.length do
     inner_count = 0
     while inner_count < src[count].length do
-      low_temp << (src[count][inner_count]).min
+      low_temp << src[count][inner_count]
+      min << low_temp.min
       inner_count += 1
     end 
     count += 1
   end
-  low_temp
+  min
 end
